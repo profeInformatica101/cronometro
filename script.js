d304aad12263cc807a61683dcf8e40d46a773555
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-04-11 17:32:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+$(function() {
+  var timer = $('#timer');
+  var hoursInput = $('#hours');
+  var minutesInput = $('#minutes');
+  var secondsInput = $('#seconds');
+  var startButton = $('#start');
+  var pauseButton = $('#pause');
+  var resetButton = $('#reset');
+  var time3Button = $('#time3');
+  var time4Button = $('#time4');
+
+  var time = 0;
+  var interval;
+
+  function updateTime() {
+    var hours = parseInt(hoursInput.val());
+    var minutes = parseInt(minutesInput.val());
+    var seconds = parseInt(secondsInput.val());
+
+    time = hours * 60 * 60 + minutes * 60 + seconds;
+    updateTimerDisplay();
+  }
+
+  function updateTimerDisplay() {
+    var hours = Math.floor(time / 3600);
+    var minutes = Math.floor((time % 3600) / 60);
+    var seconds = time % 60;
+
+    var hoursString = hours.toString().padStart(2, '0');
+    var minutesString = minutes.toString().padStart(2, '0');
+    var secondsString = seconds.toString().padStart(2, '0');
+
+    timer.text(hoursString + ':' + minutesString + ':' + secondsString);
+  }
+
+  function startTimer() {
+    interval = setInterval(function() {
+      time--;
+      if (time < 0) {
+        clearInterval(interval);
+        time = 0;
+      }
+      updateTimerDisplay();
+    }, 1000);
+  }
+
+  function pauseTimer() {
+    clearInterval(interval);
+  }
+
+  function resetTimer() {
+    pauseTimer();
+    updateTime();
+  }
+
+  function setTime3() {
+    hoursInput.val('00');
+    minutesInput.val('03');
+    secondsInput.val('00');
+    updateTime();
+  }
+
+  function setTime4() {
+    hoursInput.val('00');
+    minutesInput.val('04');
+    secondsInput.val('00');
+    updateTime();
+  }
+
+  startButton.click(startTimer);
+  pauseButton.click(pauseTimer);
+  resetButton.click(resetTimer);
+  time3Button.click(setTime3);
+  time4Button.click(setTime4);
+
+  updateTime();
+});
